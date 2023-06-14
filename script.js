74b4a431a3be98f0204ef00d226568cd456460ba
Kamil <kamil.szostak@outlook.com>
2023-06-14 23:47:57 +0200

    first part of the project

---
+++ script.js
@@ -0,0 +1,39 @@
+const quizData = [
+   {
+      question: 'which is the oldest programming language?',
+      a: 'C++',
+      b: 'Asembly',
+      c: 'Fortran',
+      d: 'Swift',
+      correct: 'b'
+   }, {
+      question: 'Which one is not a programming language?',
+      a: 'Java',
+      b: 'Python',
+      c: 'HTML',
+      d: 'Rust',
+      correct: 'c'
+   }, {
+      question: 'how many layers does the ISO/OSI reference model contain?',
+      a: '3',
+      b: '5',
+      c: '7',
+      d: '9',
+      correct: 'c'
+   }, {
+      question: 'which language is used for databases',
+      a: 'JavaScript',
+      b: 'Ruby',
+      c: 'Kotlin',
+      d: 'SQL',
+      correct: 'd'
+   }, {
+      question: 'What is the name of the company that organizes the ctf tournaments',
+      a: 'Google',
+      b: 'Apple',
+      c: 'Microsoft',
+      d: 'Amazon',
+      correct: 'a'
+   }
+
+]
